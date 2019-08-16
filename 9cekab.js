c1c29e3e5798063610e3ddd293ba766f3ff7a3a5
adryansherman <adryansherman@gmail.com>
2019-08-16 20:51:56 +0700

    highest

--- 9cekab.js
+++ 9cekab.js
@@ -1,30 +1,35 @@
 function checkAB(str) {
-    var A = []
-    var B = []
+    /*if(str.indexOf('a') === -1 || str.indexOf('b') === -1){
+        return false
+    }
+
+    var arrA = []
+    var arrB = []
     for (var i=0; i<str.length; i++) {
         if (str[i] === 'a') {
-            A.push(i)
+            arrA.push(i)
         } else if (str[i] === 'b') {
-            B.push(i)
+            arrB.push(i)
         }
     }
-    for (var j=0; j<A.length; j++) {
-        for (var k=0; k<B.length; k++) {
-            if  (Math.abs(A[j]-B[k]) > 3) {
-               return true
-           }
+    */
+    var obj = {}
+    for (var i = 0; i < str.length; i++){
+        if(obj[str[i]]=== 'a'){
+            obj.A == 
         }
     }
-    return false
+
+
     
 }
   
   // TEST CASES
-  console.log(checkAB('lane borrowed')); // true
-  console.log(checkAB('i am sick')); // false
-  console.log(checkAB('you are boring')); // true
+  //console.log(checkAB('lane borrowed')); // true
+  //console.log(checkAB('i am sick')); // false
+  //console.log(checkAB('you are boring')); // true
   console.log(checkAB('barbarian')); // true
-  console.log(checkAB('bacon and meat'));  // false
+  //console.log(checkAB('bacon and meat'));  // false
   
 
  //if (str.indexOf('a') === -1 || str.indexOf('b') === -1){return false;}
