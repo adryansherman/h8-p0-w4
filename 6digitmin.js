bc9e3f917be74d7eb5edb7c7d738c63f2a9d2f7e
adryansherman <adryansherman@gmail.com>
2019-08-24 17:55:18 +0700

    dimin

--- 6digitmin.js
+++ 6digitmin.js
@@ -1,18 +1,28 @@
 function digitPerkalianMinimum(angka) {
-    var tampung = []
-    for(var i = 1; i < angka-1; i++){
-        if(angka % i === 0){
-            
+    // you can only write your code here!
+    var digit=0
+    var tampung=[]
+    for(var i=1; i<=angka; i++){
+        if(angka % i==0){
+            tampung.push(i)
+            console.log(tampung)
         }
     }
+    var tengah=tampung.length/2
+    if(tampung.length==1){
+        digit=String(tampung[0])+String(tampung[0])
+    }else{
+        digit=String(tampung[tengah])+String(tampung[tengah-1])
+        console.log(digit)
+    }
+    return digit.length
   }
-
   
   // TEST CASES
-  /*console.log(digitPerkalianMinimum(24)); // 2
+  console.log(digitPerkalianMinimum(24)); // 2
   console.log(digitPerkalianMinimum(90)); // 3
   console.log(digitPerkalianMinimum(20)); // 2
   console.log(digitPerkalianMinimum(179)); // 4
   console.log(digitPerkalianMinimum(1)); // 2
-  */
+
 
